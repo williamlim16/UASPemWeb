bc36c36e2166d49dd96774ba2380035f8fed3094
sayasukacereal <williaml1601@gmail.com>
2020-05-18 09:24:42 +0700

    reservation half way

--- app/Movie.php
+++ app/Movie.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     protected $fillable = ['id', 'title', 'director', 'sypnosis', 'duration_min', 'age'];
-    protected $table = 'movies';
+    protected $table = 'movie';
 }
--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('movie', 'MovieController');
-Route::resource('reserve', 'ReserveController');
+Route::get('/reserve/{sid}', 'ReserveController@index');
