6b155517a66e7c95b30544bbd140210a2c2de36c
Ryukin Lika <ryukin.lika@student.umn.ac.id>
2020-05-29 01:56:19 +0800

    admin movie crud done

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,17 @@ Route::get('/reserve/{sid}', 'ReserveController@index');
 Route::post('/reserve/{sid}/store', 'ReserveController@store');
 Route::get('/reserve/{sid}/success/{seat}', 'ReserveController@success');
 
-Route::get('admin', 'AdminController@index');
+Route::get('/admin', 'AdminController@index');
+
+Route::get('/admin/movie', ['uses'=>'AdminController@movie', 'as'=>'admin.movie']);
+Route::get('/admin/movie/create', 'AdminController@movieCreate');
+Route::patch('/admin/movie/store', 'AdminController@movieInsert');
+Route::get('/admin/movie/edit/{mid}', 'AdminController@edit');
+Route::post('/admin/movie/editdetail/{mid}', 'AdminController@update');
+Route::get('/admin/movie/delete/{mid}', 'AdminController@destroy');
+
+Route::get('/admin/movie/success', 'AdminController@movieSuccess');
+
+Route::get('/admin/screening', 'AdminController@screening');
+Route::get('/admin/facility', 'AdminController@facility');
+Route::get('/admin/statistics', 'AdminController@statistics');
