d75cfee531ae302e74c6ff973dca1afb993451df
Michael Chen <56579574+michaelchen27@users.noreply.github.com>
2020-05-28 22:13:15 +0700

    Update Movie.php

--- app/Movie.php
+++ app/Movie.php
@@ -9,8 +9,4 @@ class Movie extends Model
     protected $fillable = ['id', 'title', 'director', 'sypnosis', 'time', 'age', 'posterpath'];
     protected $casts =  ['categories'=>'array', 'casts'=>'array'];
     protected $table = 'movie';
-    protected $casts = [
-        'categories' => 'array', //to print categories in movie details page.
-        'casts' => 'array' //to print casts in movie details page.
-    ];
 }
